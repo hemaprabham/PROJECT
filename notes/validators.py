0132ec125ca3797253b1127da0fa52821ef07d52
naughtygirlzz <122214796+naughtygirlzz@users.noreply.github.com>
2023-05-27 18:04:37 +0530

    Delete notes directory

--- notes/validators.py
+++
@@ -1,6 +0,0 @@
-from django.core.exceptions import ValidationError
-
-def file_size(value):
-    filesize=value.size
-    if filesize>419430400:
-        raise ValidationError("maximum size is 50 mb")
